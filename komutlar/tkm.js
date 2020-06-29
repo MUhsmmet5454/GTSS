e16b52ac4157391b250f22f82ba946de58e0b670
MUhsmmet5454 <67574478+MUhsmmet5454@users.noreply.github.com>
2020-06-29 09:05:21 +0300

    Create tkm.js

---
+++ komutlar/tkm.js
@@ -0,0 +1,153 @@
+
+
+Save New Duplicate & Edit Just Text 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+const
+    rps = [
+        'makas',
+        'taş',
+        'kağıt'
+    ],
+    rpsF = (userAns, botAns) => {
+        let choice = userAns,
+            botChoice = botAns;
+        if (choice === 'taş') {
+            if (botChoice === 'makas') {
+                return 'won';
+            } else if (botChoice === 'kağıt') {
+                return 'Kaybetin';
+            }
+
+            return 'draw';
+        } else if (choice === 'kağıt') {
+            if (botChoice === 'taş') {
+                return 'lost';
+            } else if (botChoice === 'makas') {
+                return 'Kazandın';
+            }
+
+            return 'draw';
+        } else if (choice === 'makas') {
+            if (botChoice === 'taş') {
+                return 'lost';
+            } else if (botChoice === 'kağıt') {
+                return 'Kazandın';
+            }
+
+            return 'draw';
+        }
+    };
+
+exports.run = async (client, msg, args) => {
+    if (!args[0]) {
+        return msg.channel.send('Lütfen seçimini yap taş, kağıt yada makas & tkm <taş,kağıt,makas>');
+    }
+    let choice = args[0].toLowerCase();
+    choice = choice === 't' ? 'taş' : choice;
+    choice = choice === 'k' ? 'kağıt' : choice;
+    choice = choice === 'm' ? 'makas' : choice;
+    if (!rps.includes(choice)) {
+        return msg.channel.send('Lütfen seçimini yap taş, kağıt yada makas & tkm <t,k,m>');
+    }
+    let rand = Math.floor(Math.random() * 3);
+    let botChoice = rps[rand];
+    let result = rpsF(choice, botChoice);
+    let answer = '';
+
+    if (result === 'won') {
+        answer = ':trophy: Başarılı, sen **Kazandın** :trophy: \nSenin Seçtiği: `' + choice + '` | Bot\'un Seçtiği: `' + botChoice + '`';
+    } else if (result === 'lost') {
+        answer = ':x: Bidakine **Kaybetin Dostum** :x: \nSenin Seçtiğin: `' + choice + '` | Bot\'s Seçtiği: `' + botChoice + '`';
+    } else if (result === 'draw') {
+        answer = ':neutral_face: Sonuç **Berabere** :neutral_face:\nSenin Seçimin: `' + choice + '` | Bot\'un Seçimi: `' + botChoice + '`';
+    }
+
+    msg.channel.send(answer);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+ };
+ 
+ exports.help = {
+ name: 'tkm',
+ description: 'Taş kağıt makas oyununu oynar.',
+ usage: 'tkm'
+ }
